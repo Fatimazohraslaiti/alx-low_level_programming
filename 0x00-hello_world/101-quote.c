340c0e4fc04d39747ca22e878075bbd57c5507a1
Fatimazohraslaiti <fatimaslaiti198@gmail.com>
2023-03-19 19:24:03 -0700

    aaaa

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char *gett = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, gett, 59);
+return (0);
+}
