11ed7ca3ee7629aba41bbf93e9a90c29be7f39e8
Fatimazohraslaiti <fatimaslaiti198@gmail.com>
2023-03-22 07:25:39 -0700

    aaaa

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add  - add a and b
+ *@a: first num
+ *@b: second num
+ * Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
