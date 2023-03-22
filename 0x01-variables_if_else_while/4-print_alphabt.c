797d21dbe1a624f082a76ceed3cd4bc06b609b74
Fatimazohraslaiti <fatimaslaiti198@gmail.com>
2023-03-22 13:11:35 -0700

    aaaa

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	char l = 'a';
+	char L = 'A';
+
+	while (l <= 'z')
+	{
+	putchar(l);
+	l++;
+	}
+	while (L <= 'Z')
+	{
+	putchar(L);
+	L++;
+	}
+	putchar('\n');
+	return (0);
+}
