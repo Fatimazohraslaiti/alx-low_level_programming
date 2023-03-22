21a157b32e23c6687141d3339b4c14c45b8cedbd
Fatimazohraslaiti <fatimaslaiti198@gmail.com>
2023-03-21 20:04:26 -0700

    aaaa

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,38 @@
 #include "main.h"
+
 /**
-* main - check the code
-*
-* Return: Always 0.
-*/
-int main(void)
+ *jack_bauer - prints every minute of the day
+ *
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
 {
-	jack_bauer();
-	return (0);
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (i == 2 && j == 4)
+			{
+				return;
+			}
+			for (k = 0; k <= 5; k++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+				_putchar(i + '0');
+				_putchar(j + '0');
+				_putchar(':');
+				_putchar(k + '0');
+				_putchar(l + '0');
+				_putchar('\n');
+				}
+			}
+		}
+	}
 }
