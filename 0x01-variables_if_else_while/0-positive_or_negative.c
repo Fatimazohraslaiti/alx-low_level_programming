37e5b26a282847dd3ea92cfe4d0e5336d05f4c01
Fatimazohraslaiti <fatimaslaiti198@gmail.com>
2023-03-22 11:39:37 -0700

    aaaa

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * add  - add a and b
+ *@a: first num
+ *@b: second num
+ * Return: Always 0.
  */
-int main(void)
+
+int add(int a, int b)
 {
-	int n;
-	
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	int result;
 
-
-	if (n > 0)
-                printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);	return (0);
+	result = a + b;
+	return (result);
 }
