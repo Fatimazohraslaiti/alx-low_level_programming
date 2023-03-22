d949a1502e7987f57165cb0aca5c15aebef91769
Fatimazohraslaiti <fatimaslaiti198@gmail.com>
2023-03-21 18:27:39 -0700

    aaaa

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - check if is lower of not.
+ *@c: char to check
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
